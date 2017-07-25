21411b430a980519e58311ab31d9ac00d1edd50d
910JQK <v910JQK@gmail.com>
2017-07-25 15:31:39 +0800

    More comment

--- core.hpp
+++ core.hpp
@@ -134,11 +134,24 @@ namespace GwanKei {
       std::list result;
       if(group == Central) {
 	if(y == 0 && x == 0) {
+	  /**
+	            ||
+	        = [0000] =
+		    ||
+	   */
 	  result.push(Bound(Cell(0110), South));
 	  result.push(Bound(Cell(0220), East));
 	  result.push(Bound(Cell(0330), North));
 	  result.push(Bound(Cell(0440), West));
 	} else if (y == x) {
+	  /**
+	                  [0000]
+	                    ||
+	     [0(y-1)y0] = [0yy0] = [0y(y+1)0]
+	                    ||
+	                  [y130]
+			   
+	   */
 	  result.push(Bound(Cell(0000), opposite_orient(y)) );
 	  result.push(Bound(Cell(0, y, next_orient(y)), next_orient(y)) );
 	  result.push(Bound(Cell(0, prev_orient(y), y), prev_orient(y)) );
@@ -164,6 +177,12 @@ namespace GwanKei {
 	  /* Horizontal */
 	  if(y == 1 || y == 5) {
 	    /* Horizontal Railway (to y,2) */
+	    /**
+	       [11] = [12]
+	        ..
+	       [51] = [52]
+	        ..
+	     */
 	    result.push(
 			Bound(
 			      Cell(group, y, 2, lr),
@@ -172,19 +191,54 @@ namespace GwanKei {
 			);
 	  } else {
 	    /* Horizontal Road (to y,2) */
+	    /**
+	       [11]
+	       [21] - (22)
+	       [31] - [32]
+	       [41] - (42)
+	       [51]
+	       [61] - {62}
+	     */
 	    result.push(Bound(Cell(group, y, 2, lr)) );
 	  }
 	  /* Vertical*/
 	  if(y >= 1 && y <= 4) {
 	    /* Downward Railway (to y+1,1) */
+	    /**
+	       [11]
+	        |/
+	       [21]
+	        |/
+	       [31]
+	        |/
+	       [41]
+	        |/
+	       [51]
+	     */
 	    result.push(Bound(Cell(group, y+1, 1, lr), group));
 	  }
 	  if(y >= 2 && y <= 5) {
 	    /* Upward Railway (to y-1,1) */
+	    /**
+	       [11]
+	        |\
+	       [21]
+	        |\
+	       [31]
+	        |\
+	       [41]
+	        |\
+	       [51]
+	     */
 	    result.push(Bound(Cell(group, y-1, 1, lr), group));
 	  }
 	  if(y == 1) {
 	    /* Upward Railway (to Central) */
+	    /**
+	       [0(G-1)G0] ... [0G(G+1)0]
+	           ||             ||
+	       [G  11  L] ... [G  11  R]
+	     */
 	    if(get_lr() == Left) {
 	      result.push(
 			  Bound(
@@ -201,6 +255,11 @@ namespace GwanKei {
 			  );
 	    }
 	    /* Side Railway (to 11 of next and prev) */
+	    /**
+	       [(G-1)11R] ..................... [(G+1)11L]
+	                \\                     //
+	             [G  11  L] ......... [G  11  R]
+	     */
 	    result.push(
 			Bound(
 			      Cell(next_orient(group), 1, 1, Left),
@@ -218,21 +277,45 @@ namespace GwanKei {
 	  }
 	  if(y == 5) {
 	    /* Road to 61 */
+	    /**
+	       [51]
+	        |
+	       [61]
+	     */
 	    result.push(Bound(Cell(group, 6, 1, lr)) );
 	  }
 	  /* Oblique */
 	  if(y == 1 || y == 3) {
 	    /* Road to 22 */
+	    /**
+	       [11]
+	           \
+		    (22)
+	           /
+	       [31]
+	     */
 	    result.push(Bound(Cell(group, 2, 2, lr)) );
 	  }
 	  if(y == 3 || y == 5) {
 	    /* Road to 42 */
+	    /**
+	       [31]
+	           \
+		    (42)
+	           /
+	       [51]
+	     */
 	    result.push(Bound(Cell(group, 4, 2, lr)) );
 	  }
 	} else if (x == 2) {
 	  /* Horizontal */
 	  if(y == 1 || y == 5) {
 	    /* Horizontal Railway */
+	    /**
+	       [11] = [12] = [13]
+	        ..     ..     ..
+	       [51] = [52] = [53]
+	     */
 	    result.push(
 			Bound(
 			      Cell(group, y, 1, lr),
@@ -247,21 +330,62 @@ namespace GwanKei {
 			);	    
 	  } else {
 	    /* Horizontal Road to y,1 and y,3 */
+	    /**
+	        ..     ..     ..
+	       [21] - (22) - [23]
+	       [31] - [32] - (33)
+	       [41] - (42) - [43]
+	        ..     ..     ..
+	       [61] - {62} - [63]
+	     */
 	    result.push(Bound(Cell(group, y, 1, lr)) );
 	    result.push(Bound(Cell(group, y, 3, lr)) );
 	  }
 	  /* Vertical */
 	  if(y >= 2 && y <= 6) {
 	    /* Upward Road */
+	    /**
+	       [12]
+	        ^
+	       (22)
+	        ^
+	       [32]
+	        ^
+	       (42)
+	        ^
+	       [52]
+	        ^
+	       {62}
+	     */
 	    result.push(Bound(Cell(group, y-1, 2, lr)) );
 	  }
 	  if(y >= 1 && y <= 5) {
 	    /* Downward Road */
+	    /**
+	       [12]
+	        \
+	       (22)
+	        \
+	       [32]
+	        \
+	       (42)
+	        \
+	       [52]
+	        \
+	       {62}
+	     */
 	    result.push(Bound(Cell(group, y+1, 2, lr)) );
 	  }
 	  /* Oblique */
 	  if(y == 2 || y == 4) {
 	    /* Roads from Camp */
+	    /**
+	       [11]  ..  [13]
+	           \    /
+	        ..  (22)  ..
+	           /    \
+	       [31]  ..  (33)
+	    */
 	    result.push(Bound(Cell(group, y+1, x+1, lr)) );
 	    result.push(Bound(Cell(group, y+1, x-1, lr)) );
 	    result.push(Bound(Cell(group, y-1, x+1, lr)) );
@@ -272,16 +396,35 @@ namespace GwanKei {
 	  /* Horizontal */
 	  if(y == 1 || y == 5) {
 	    /* Horizontal Railway to y,2 */
+	    /**
+	       [12L] = [13] = [12R]
+	        ...     ..     ...
+	       [52L] = [53] = [52R]
+	        ...     ..     ...
+	     */
 	    result.push(Bound(Cell(group, y, 2, Left), prev_orient(group)) );
 	    result.push(Bound(Cell(group, y, 2, Right), next_orient(group)) );
 	  } else {
 	    /* Horizontal Road to y,2 */
+	    /**
+	        ...     ..     ...
+	       (22L) - [23] - (22R)
+	       [32L] - (33) - [32R]
+	       (42L) - [43] - (42R)
+	        ...     ..     ...
+	       {62L} - [63] - {62R}
+	     */
 	    result.push(Bound(Cell(group, y, 2, Left)) );
 	    result.push(Bound(Cell(group, y, 2, Right)) );
 	  }
 	  /* Vertical */
 	  if(y == 1) {
 	    /* Railway to Central */
+	    /**
+	       [0GG0]
+	         ||
+	       [G130]
+	     */
 	    result.push(
 			Bound(
 			      Cell(Central, group, group, 0),
@@ -291,25 +434,68 @@ namespace GwanKei {
 	  }
 	  if(y >= 2 && y <= 6) {
 	    /* Upward Road */
+	    /**
+	       [13]
+	        ^
+	       [23]
+	        ^
+	       (33)
+	        ^
+	       [43]
+	        ^
+	       [53]
+	        ^
+	       [63]
+	     */
 	    result.push(Bound(Cell(group, y-1, 3)) );
 	  }
 	  if(y >= 1 && y <= 5) {
 	    /* Downward Road */
+	    /**
+	       [13]
+	        \
+	       [23]
+	        \
+	       (33)
+	        \
+	       [43]
+	        \
+	       [53]
+	        \
+	       {63}
+	     */
 	    result.push(Bound(Cell(group, y+1, 3)) );
 	  }
 	  /* Oblique */
 	  if(y == 1) {
 	    /* 13 to 22 */
+	    /**
+	        ... [13] ...
+		   /    \
+	       (22L)    (22R)
+	     */
 	    result.push(Bound(Cell(group, y+1, x-1, Left)) );
 	    result.push(Bound(Cell(group, y+1, x-1, Right)) );
 	  }
 	  if(y == 1) {
 	    /* 53 to 42 */
+	    /**
+	       (42L)    (42R)
+	           \    /
+	        ... [53] ...
+	     */
 	    result.push(Bound(Cell(group, y-1, x-1, Left)) );
 	    result.push(Bound(Cell(group, y-1, x-1, Right)) );
 	  }
 	  if(y == 3) {
 	    /* Center Camp */
+	    /**
+	       (22L) .. (22R)
+	           \    /
+	        ... (33) ...
+	           /    \
+	       (42L) .. (42R)
+	     */
 	    result.push(Bound(Cell(group, y+1, x-1, Left)) );
 	    result.push(Bound(Cell(group, y-1, x-1, Left)) );
 	    result.push(Bound(Cell(group, y+1, x-1, Right)) );
